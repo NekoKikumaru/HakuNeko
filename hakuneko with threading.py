bc00e1d5f08b3c54f4e1dd966da0cc35377c3791
NekoKikumaru <61583022+NekoKikumaru@users.noreply.github.com>
2022-11-13 19:13:26 +0630

    Add python file for threading via upload

---
+++ hakuneko with threading.py
@@ -0,0 +1,73 @@
+import cloudscraper, inquirer, json, os, threading
+from bs4 import BeautifulSoup
+
+class HakuNeko:
+
+    def progress(self, url):
+        print('Currently scraping: '+url)
+
+    def url2soup(self, url):
+        scraper = cloudscraper.CloudScraper()
+        html = scraper.get(url).text
+        soup = BeautifulSoup(html, features='html.parser')
+        return soup
+
+    def hakuneko(self, comics, extension):
+        comics = sorted(comics, key=lambda c: c['title'])
+        f = open('hakuneko.mangas.'+extension, 'w+', encoding='iso-8859-8')
+        f.write(json.dumps(comics))
+        f.close()
+        print('hakuneko.mangas.'+extension+' is in '+os.getcwd())
+
+class MangaGo:
+
+    def __init__(self):
+        super().__init__()
+        self.comics = []
+        self.run()
+    
+    def generate(self):
+        base = 'https://www.mangago.me/list/directory/all/'
+        result = None
+        while result is None:
+            soup = HakuNeko().url2soup(base+'1/')
+            div = soup.find('div', {'class': 'pagination'})
+            result = div
+        pages = int(div['total'])
+        pages = [base+str(p+1)+'/' for p in list(range(pages))]
+        return pages
+
+    def scrape(self, pages):
+        for page in pages:
+            HakuNeko().progress(page)
+            result = 0
+            while result == 0:
+                soup = HakuNeko().url2soup(page)
+                aList = soup.findAll('a', {'class': 'lazy'})
+                for a in aList:
+                    self.comics.append({'id': a['href'], 'title': a['title']})
+                result = len(aList)
+
+    def bg_thread(self, pages):
+        threads = []
+        chunks = 10
+        for p in range(0, len(pages), chunks):
+            t = threading.Thread(target=self.scrape, args=[pages[p:p+chunks]])
+            threads.append(t)
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+        return self.comics
+
+    def run(self):
+        pages = self.generate()
+        comics = self.bg_thread(pages)
+        HakuNeko().hakuneko(comics, 'mangago')
+
+if __name__ == '__main__':
+    sources = ['Mangago']
+    question = [inquirer.List('source', message="Select the source to generate hakuneko data", choices=list(sources))]
+    answer = inquirer.prompt(question)
+    if answer['source'] == sources[0]:
+        MangaGo()
